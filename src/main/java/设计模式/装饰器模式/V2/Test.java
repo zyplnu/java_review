8eacf7bdc771cae4953f08e269a30c95573d5b32
zyplnu <429510037@qq.com>
2022-03-20 22:36:38 +0800

    add

--- src/main/java/设计模式/装饰器模式/V2/Test.java
+++ src/main/java/设计模式/装饰器模式/V2/Test.java
@@ -3,7 +3,7 @@ package 设计模式.装饰器模式.V2;
 public class Test {
     public static void main(String[] args) {
         BatterCake batterCake = new BaseBatterCake();
-        batterCake = new EggBatterCakeDecerator(batterCake)
+        batterCake = new EggBatterCakeDecerator(batterCake);
 
 
         System.out.println(batterCake.getMsg() + "," + batterCake.getPrice());
